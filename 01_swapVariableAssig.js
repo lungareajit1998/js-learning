6717cc9e04fd9b2aa15ef4b5419b883f4a5f0de6
lungareajit1998 <lungareajit1998@gmail.com>
2024-03-08 18:49:44 +0530

    Completed two assignments of JS
    Completed two assignments of JS

---
+++ 01_swapVariableAssig.js
@@ -0,0 +1,25 @@
+console.log("***** Part 1 *****");
+console.log("====== Before Swap =======");
+var sweety= "Sweety"
+var cutie="Cutie"
+console.log("sweety:",sweety,"cutie:",cutie);
+
+console.log("====== After Swap =======");
+temp= sweety
+sweety = cutie
+cutie = temp
+console.log("sweety:",sweety,"cutie:",cutie);
+
+console.log("***** Part 2 *****");
+console.log("====== Before Swap ======");
+var num1= 100 ;
+var num2= 200 ;
+var num3= 300 ;
+console.log("num1:",num1,"num2:",num2,"num3:",num3);
+
+console.log("====== After Swap ======");
+temp=num1
+num1=num2
+num2=num3
+num3=temp
+console.log("num1:",num1,"num2:",num2,"num3:",num3);
---
+++ 01_variableAssign.js
@@ -0,0 +1,35 @@
+console.log("===== step 1 =====");
+
+var myFullName = "Ajit Lungare"; //Variable Declaration
+console.log("My Full name before initialization is:", myFullName);
+var myFullName = "Ajit Ankush Lungare";
+console.log("My Full Name After update:", myFullName);
+
+console.log("===== step 2 =====");
+
+var myLove="I Love Only JS";
+console.log("My Love:",myLove);
+
+console.log("===== step 3 =====");
+
+var firstName="Ajit";
+console.log("First Name:",firstName);
+
+var lastName="Lungare";
+console.log("Last Name:",lastName);
+
+var age="25";
+console.log("Age:",age);
+
+var address="Bhagya nagar, Nanded";
+console.log("Address:",address);
+
+var pincode="431605";
+console.log("Pincode:",pincode);
+
+var gender="Male";
+console.log("Gender:",gender);
+
+var maritalStatus="Unmarried";
+console.log("Marital Status:",maritalStatus);
+
