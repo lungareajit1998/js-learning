e9d825f570fe381338a78e02bb61817d151c0ee6
lungareajit1998 <lungareajit1998@gmail.com>
2024-03-12 11:59:27 +0530

    create 03_stringBasicAssign01.js
    create 03_stringBasicAssign01.js

---
+++ 03_stringBasicAssign01.js
@@ -0,0 +1,17 @@
+
+console.log("========== Step 1 ==========");
+function stringBasics() {
+    var  company
+    company = "TCS"
+    console.log("My Favorite Company is:",company);
+}
+stringBasics()
+
+console.log("========== Step 2 ==========");
+function myhobbies(yourhobby1 , yourhobby2, yourhobby3){
+    console.log("My Hobbies: ", yourhobby1, yourhobby2, yourhobby3);
+    var result = yourhobby1+yourhobby2+yourhobby3;
+    var sum = result.length;
+    console.log("The Length is:",sum);  
+}
+myhobbies("Writing"," Reading"," Playing")
